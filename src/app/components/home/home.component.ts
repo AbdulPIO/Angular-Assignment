63f9f696c01ad78fbe5cf22a814bc111f59b5a2f
AbdulPIO <abdul.khan@programmers.io>
2025-05-02 10:09:37 +0530

    Added Sorting and Pagination

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,6 +4,7 @@ import { RouterModule, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ExamService } from '../../services/exam.service';
 import { Exam } from '../../models/exam';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -17,6 +18,14 @@ export class HomeComponent implements OnInit {
   filteredExams: Exam[] = [];
   searchTerm: string = '';
 
+  // variable declaration for sorting and pagination
+  currentPage = 1;
+  pageSize = 5;
+  totalPages = 1;
+
+  sortColumn: keyof Exam = 'examName';
+  sortDirection: 'asc' | 'desc' = 'asc';
+
   constructor(
     private examService: ExamService,
     private router: Router
@@ -26,11 +35,16 @@ export class HomeComponent implements OnInit {
     this.loadExams();
   }
 
+  /**
+   * Loads all exams from the API and applies default filter, sort, and pagination.
+   */
   loadExams() {
     this.examService.getAllExams().subscribe({
       next: (data: Exam[]) => {
         this.exams = data;
-        this.filteredExams = data;
+        this.sortExams();
+        this.applyFilterandPagination();
+        // this.filteredExams = data;
       },
       error: () => {
         alert('Failed to load exams');
@@ -38,12 +52,47 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  applyFilter() {
+  applyFilterandPagination() {
     const search = this.searchTerm.toLowerCase();
-    this.filteredExams = this.exams.filter(exam =>
+    const filtered = this.exams.filter(exam =>
       exam.examName.toLowerCase().includes(search) ||
       exam.examKey.toLowerCase().includes(search)
     );
+
+    this.totalPages = Math.ceil(filtered.length / this.pageSize);
+    this.currentPage = Math.min(this.currentPage, this.totalPages || 1);
+
+    const start = (this.currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+
+    this.filteredExams = filtered.slice(start, end);
+  }
+
+  /**
+   * Sorts the full exam list based on a column and direction, then reapplies filter and pagination
+   * @param column The key of the exam object to sort by
+   */
+  onSort(column: keyof Exam) {
+    if (this.sortColumn === column) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortColumn = column;
+      this.sortDirection = 'asc';
+    }
+
+    this.sortExams();
+    this.applyFilterandPagination();
+  }
+
+  /**
+   * Applies sorting on th full list of exams.
+   */
+  private sortExams() {
+    this.exams.sort((a, b) => {
+      const valA = a[this.sortColumn]!.toString().toLowerCase() || '';
+      const valB = b[this.sortColumn]!.toString().toLowerCase() || '';
+      return this.sortDirection === 'asc' ? valA.localeCompare(valB) : valB.localeCompare(valA);
+    });
   }
 
   onEdit(exam: Exam) {
@@ -61,7 +110,9 @@ export class HomeComponent implements OnInit {
       this.examService.deleteExam(exam.id).subscribe({
         next: () => {
           this.exams = this.exams.filter(e => e.id !== exam.id);
-          this.filteredExams = this.filteredExams.filter(e => e.id !== exam.id);
+          this.sortExams();
+          this.applyFilterandPagination();
+          // this.filteredExams = this.filteredExams.filter(e => e.id !== exam.id);
           alert('Exam deleted successfully');
         },
         error: () => {
@@ -69,5 +120,20 @@ export class HomeComponent implements OnInit {
         }
       });
     }
+  }
+
+  goToPage(page: number) {
+    if (page >= 1 && page <= this.totalPages) {
+      this.currentPage = page;
+      this.applyFilterandPagination();
+    }
+  }
+
+  nextPage() {
+    this.goToPage(this.currentPage + 1);
+  }
+
+  previousPage() {
+    this.goToPage(this.currentPage - 1);
   }
 } 
